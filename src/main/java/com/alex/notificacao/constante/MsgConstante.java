dbac415ac07976ba4c014a78eecdb85e0f35135e
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-24 21:49:40 -0300

    estruturando a api

---
+++ src/main/java/com/alex/notificacao/constante/MsgConstante.java
@@ -0,0 +1,6 @@
+package com.alex.notificacao.constante;
+
+public class MsgConstante {
+
+    public  static final String PROPOSTA_EM_ANALISE = "Prezado %s, sua proposta foi recebida. Breve retornaremos o contato";
+}
---
+++ src/main/java/com/alex/notificacao/listener/PropostaPendenteListener.java
@@ -0,0 +1,22 @@
+package com.alex.notificacao.listener;
+import com.alex.notificacao.constante.MsgConstante;
+import com.alex.notificacao.domain.Proposta;
+import com.alex.notificacao.service.NotificacaoSnsService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaPendenteListener {
+
+    @Autowired
+    private NotificacaoSnsService snsService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta) {
+        String msg = String.format(MsgConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario());
+        snsService.notificar(msg);
+    }
+
+
+}
